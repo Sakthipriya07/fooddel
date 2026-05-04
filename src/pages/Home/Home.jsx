151c0cb002a8927e935eaa45eb8b696483f83560
Sakthipriya07 <166027450+Sakthipriya07@users.noreply.github.com>
2026-05-04 21:28:21 +0530

    Fix import case for FoodDisplay components
    second commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import AppDownload from '../../components/AppDownload/AppDownload'
-import FoodDisplay from '../../components/FoodDisplay/Fooddisplay'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
 
